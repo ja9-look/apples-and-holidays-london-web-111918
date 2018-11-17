e1c62a1f4085d729288e0596e595367e0df4d5fd
Janine Luk <janineluk95@gmail.com>
2018-11-17 15:48:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq_holiday = []
     holiday_hash.each do |season, holidays|
-      
+         holidays.each do |holiday, supplies|
+   
 
 end
 
