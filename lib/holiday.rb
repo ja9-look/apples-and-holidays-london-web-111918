7213441b497095b840d4b2980b31e53c079ce945
Janine Luk <janineluk95@gmail.com>
2018-11-17 15:48:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,8 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |season, holidays|
       holidays.each do |holiday, supplies|
         supplies.each do |item|
-          if item == "bbq"
+          if item == "BBQ"
+            
 
 end
 
